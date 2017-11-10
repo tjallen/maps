39d5789896a9c63807da3c2209db3837a5a83001
Thom Allen <thomwork@gmail.com>
2017-11-10 08:59:28 +0000

    Import fake API keys from json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import keys from './keys';
 
 class App extends Component {
   render() {
+    const keyInfo = `fake keys: ${keys.API_KEY}, ${keys.APP_ID}`;
     return (
       <div>
         <p>hello world</p>
+        <p>{keyInfo}</p>
       </div>
     );
   }
