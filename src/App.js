d47a42104d82d750c9d5f75f859aa26d98ae54f2
Thom Allen <thomwork@gmail.com>
2017-11-10 09:02:05 +0000

    Add src/keys.json to gitignore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import keys from './keys';
+// import keys from './keys';
 
 class App extends Component {
   render() {
-    const keyInfo = `fake keys: ${keys.API_KEY}, ${keys.APP_ID}`;
+    // const keyInfo = `API key: ${keys.API_KEY}, App ID: ${keys.APP_ID}`;
     return (
       <div>
         <p>hello world</p>
-        <p>{keyInfo}</p>
+        {/* <p>{keyInfo}</p> */}
       </div>
     );
   }
